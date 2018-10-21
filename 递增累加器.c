9af7fb7ffa2471b1fed6b76f12a71dd1162951a0
AcoderWu <43341200+ACoderWu@users.noreply.github.com>
2018-10-21 16:14:45 +0800

    An Increasing accumulator
    可以累加传入的两个参数之间的递增

--- 递增累加器.c
+++ 递增累加器.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int sum(int begin, int end);
+int sum(int begin, int end);	//函数的声明，如果没有这个声明，那么函数定义就必须写在函数第一次使用的前面；
 
 int main()
 {
@@ -8,15 +8,26 @@ int main()
     int b = 0;
 	int s = 0;
 
-    scanf("%d",&a);
-	scanf("%d",&b);
-    s = sum(a,b);
-    printf("%d",s);
+    do
+	{
+		scanf("%d",&a);
+		scanf("%d",&b);
+		if(a < b)
+		{
+			s = sum(a,b);	//如果前面没有函数声明或定义的话，这里会产生一个warning，如果前面没有声明或定义且后面函数类型不为 int function(int arg, int arg) 时，会产生error；
+			printf("%d",s);
+		}
+		else
+		{
+			break;
+		}
+	}
+	while(b > a);
 
     return 0;
 }
 
-int sum(int begin, int end)
+int sum(int begin, int end)	//函数的定义
 {
 	int i = 0;
 	int sum = 0;
